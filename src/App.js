073ed54e412d9b491d92256640c146d99cd04156
BenjaminDagg <bdagg@csumb.edu>
2018-09-18 00:58:47 -0700

    added socket on client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { openSocket } from 'socket.io-client';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    var socket = openSocket('https://draw-with-me-dagg.herokuapp.com:8080')
+  }
   render() {
+
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
