ee22d9ef18adfbbfb4436811d2ccc7a844bcc8dc
BenjaminDagg <bdagg@csumb.edu>
2018-09-18 01:12:38 -0700

    created basic socket server

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 var express = require('express');
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
 
 var cors = require('cors');
 app.use(cors());
+
+
+const io = require('socket.io')();
+
+io.on('connection', (client) => {
+    console.log('user connected');
+});
 
 app.use( express.static( __dirname + `/build` ) );
 
@@ -13,8 +19,8 @@ app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-});
+
+
+io.listen(5000);
 
 app.listen(process.env.PORT || 8080 ,() => console.log('server listening'));
--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { openSocket } from 'socket.io-client';
+import  openSocket  from 'socket.io-client';
 import './App.css';
 
 class App extends Component {
 
   componentDidMount() {
-    var socket = openSocket('https://draw-with-me-dagg.herokuapp.com:8080')
+    var socket = openSocket('http://localhost:5000');
   }
+
   render() {
 
 
 
     return (
       <div className="App">
-
+        <h1>hello</h1>
       </div>
     );
   }
